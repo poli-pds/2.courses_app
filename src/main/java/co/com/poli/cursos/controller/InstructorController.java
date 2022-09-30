df54f3cdf9d6a9584df93474a847cefb817bb1a3
Giovanny Vargas <yovaz05@gmail.com>
2022-09-29 19:38:39 -0500

    clase 29-09-2022

--- src/main/java/co/com/poli/cursos/controller/InstructorController.java
+++ src/main/java/co/com/poli/cursos/controller/InstructorController.java
@@ -1,4 +1,27 @@
 package co.com.poli.cursos.controller;
 
+import co.com.poli.cursos.persistence.entity.Instructor;
+import co.com.poli.cursos.services.InstructorService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/instructors")
+@RequiredArgsConstructor
 public class InstructorController {
+
+    private final InstructorService instructorService;
+
+    @PostMapping
+    public Instructor save(@RequestBody Instructor instructor) {
+        return instructorService.save(instructor);
+    }
+
+    @GetMapping
+    public List<Instructor> findAll() {
+        return instructorService.findAll();
+    }
+
 }
